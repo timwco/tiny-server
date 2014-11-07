21ca23cfa10afd67ead1bbec0826d9fd10ba0a76
Tim Whitacre <tim@timw.co>
2014-11-07 09:08:14 -0500

    Upped server to 25

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.param('collectionName', function(req, res, next, collectionName){
 
 // GET /collections/:collectionName
 app.get('/collections/:collectionName', function(req, res) {
-  req.collection.find({},{limit:20, sort: [['_id',-1]]}).toArray(function(e, results){
+  req.collection.find({},{limit:25, sort: [['_id',-1]]}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
